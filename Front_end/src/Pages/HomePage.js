c2ad7d44a256f76e08375b306729182130c62aea
RanyaTaoui <ranyataoui@outlook.com>
2023-05-19 12:43:08 +0200

    add Cards to the home page

--- Front_end/src/Pages/HomePage.js
+++ Front_end/src/Pages/HomePage.js
@@ -2,42 +2,56 @@ import React from "react";
 import { useState } from "react";
 import Navbar from "./Navbar";
 import "./css/homePage.css";
+import omar from '../assets/memberpictures/memberpictures/temsamaniomar.jpg';
+import soufiane from '../assets/memberpictures/memberpictures/oukattousoufiane.jpg';
+import ranya from '../assets/memberpictures/memberpictures/taouiranya.jpg';
+import hamza from '../assets/memberpictures/memberpictures/zeroualihamza.jpg';
+import saad from '../assets/memberpictures/memberpictures/elgbourisaad.jpg';
+import anas from '../assets/memberpictures/memberpictures/zakarianas.jpg';
 
 function HomePage() {
     const teamMembers = [
         {
-            name: "John Doe",
-            email: "john@example.com",
-            phone: "123-456-7890",
-            role: "Developer",
-            picture: ""
+            name: "Temsamani Omar",
+            email: "omartemsamani22@gmail.com",
+            role: "Scrum Master CTO",
+            picture: omar
         },
         {
-            name: "John Doe",
-            email: "john@example.com",
-            phone: "123-456-7890",
-            role: "Developer",
-            picture: ""
+            name: "Oukattou Soufiane",
+            email: "Soufiane321oukattou@gmail.com",
+            role: "Lead Developer",
+            picture: soufiane
         },
         {
-            name: "John Doe",
-            email: "john@example.com",
-            phone: "123-456-7890",
-            role: "Developer",
-            picture: "../assets/img/bg.jpg"
+            name: "Elgbrouri Saad",
+            email: "elgbouri.saad@gmail.com",
+            role: "Backend Developer",
+            picture: saad
         },
         {
-            name: "John Doe",
-            email: "john@example.com",
-            phone: "123-456-7890",
-            role: "Developer",
-            picture: ""
+            name: "Taoui Ranya",
+            email: "ranyataoui@outlook.com",
+            role: "Ui/Ux Designer",
+            picture:  ranya
+        },
+        {
+            name: "Zakari Anas",
+            email: "Zakari.anas2000@gmail.com",
+            role: "Backend Developer",
+            picture: anas
+        },
+        {
+            name: "Zerouali Hamza",
+            email: "H.zerouali04@gmail.com",
+            role: "Frontend Developer",
+            picture:    hamza
         },
         // Add more team members here
     ];
 
     return (
-        <div className="home-body justify-content-center min-vh-100">
+        <div className="home-body  d-flex justify-content-center justify-content-center min-vh-100">
             <Navbar />
 
             <div className="container d-flex justify-content-center align-items-center">
@@ -45,32 +59,21 @@ function HomePage() {
                     {teamMembers.map((member, index) => (
                         <div className="col-md-4" key={index}>
 
-<div class="card mb-3">
-  <div class="row g-0">
-    <div class="col-md-4">
-      {/* <img src="..." class="img-fluid rounded-start" alt="..."> */}
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-                            <div className="card">
-                                <img src={member.picture} className="card-img-top" alt={member.name} />
-                                <div className="card-body">
-                                    <h5 className="card-title">{member.name}</h5>
-                                    <p className="card-text">{member.email}</p>
-                                    <p className="card-text">{member.phone}</p>
-                                    <p className="card-text">{member.role}</p>
+                            <div class="card mb-3">
+                                <div class="row g-0">
+                                    <div class="col-md-4">
+                                    <img src={member.picture} class="img-fluid rounded-start" alt="member picture"/> 
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body">
+                                            <h5 class="card-title">{member.name}</h5>
+                                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
+
                         </div>
                     ))}
                 </div>
